79109d97437c140077beeafbdf413ca48f9a37f6
Medha Khatri <81421540+medhhaa@users.noreply.github.com>
2025-06-23 12:57:17 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data-processing-pipeline",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "pyspark",
+        "python-dotenv",
+        "PyYAML",
+        "pytest",
+        "black",
+        "numpy",
+        "scikit-learn",
+        "prophet",
+        "pymongo",
+    ],
+)
